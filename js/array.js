d8e6e8eb22d06d8fa974e54afed82ee5410898b6
DESKTOP-NUOPUIF\ITPS <jw7812@naver.com>
2023-08-02 17:46:50 +0900

    front 커밋 (2023.08.02 5:46pm)

---
+++ js/array.js
@@ -0,0 +1,68 @@
+// const user = { 
+//     key: value // js에서는 key 와 value 값을 넣고 닫으면 객체이다
+// };
+
+window.onload = () => {
+
+    const numbers = [1,2,3,4,5];
+
+    // const fx = (n) => {
+    //     return n * 2;
+    // }
+
+    // map : 새로운 배열을 만들어 주는 것
+    const numbers2 = numbers.map((n) => {
+        return n * 2;
+    });
+
+    console.log(numbers.join("-"));
+    console.log(numbers2);
+
+    loadUserList();
+}
+
+// List
+const userList = new Array();
+userList.push({username : "aaa", password : "1111"});
+userList.push({username : "bbb", password : "2222"});
+userList.push({username : "ccc", password : "3333"});
+userList.push({username : "ddd", password : "4444"});
+userList.push({username : "eee", password : "5555"});
+userList.push({username : "fff", password : "6666"});
+
+const loadUserList = () => {
+    const userListTbody = document.querySelector(".user-list");
+
+    for(let i = 0; i < userList.length; i++) {
+        userListTbody.innerHTML += `
+            <tr>
+                <td>${userList[i].username}</td>
+                <td>${userList[i].password}</td>
+            </tr>
+        `;
+    }
+
+    // forEach
+    const mapTest = userList.map((user) => {
+        return `
+            <tr>
+                <td>${user.username}</td>
+                <td>${user.password}</td>
+            </tr>
+        `
+    });
+
+    console.log(mapTest)
+
+    userListTbody.innerHTML = mapTest.join("");
+
+};
+
+// const loadUserList = () => {
+//     const userListTbody = document.querySelector(".user-list");
+
+//     userListTbody.innerHTML = userList.map(user => {
+//         return `
+//         `
+//     }).join("");
+// }
---
+++ js/array2.js
@@ -0,0 +1,53 @@
+window.onload = () => {
+
+    loadBookList();
+
+};
+
+const bookList = new Array();
+
+bookList.push({
+    bookName: "도서명1",
+    author: "저자1",
+    publisher: "출판사1"
+});
+bookList.push({
+    bookName: "도서명2",
+    author: "저자2",
+    publisher: "출판사2"
+});
+bookList.push({
+    bookName: "도서명3",
+    author: "저자3",
+    publisher: "출판사3"
+});
+
+
+
+const loadBookList = () => {
+    const bookList01 = document.querySelector(".book-list");
+
+    bookList01.innerHTML = bookList.map(book => {
+        return `
+            <li>
+                <span>${book.bookName}</span> |
+                <span>${book.author}</span> |
+                <span>${book.publisher}</span>
+            </li>
+        `;
+
+    }).join("");
+
+};
+
+// 도서를 추가 하는 작업
+const addBookOnClickHandle = () => {
+    const book = {
+        bookName: document.querySelector(".book-name").vaiue, // input에 있는 value를 가져온다.
+        author: document.querySelector(".author").vaiue,
+        publisher: document.querySelector(".publisher").vaiue
+    }
+
+    bookList.push(book);
+    loadBookList();
+};
